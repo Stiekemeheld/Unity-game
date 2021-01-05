925b31fba9dbb80a4b6457acf865b8e3e5042a10
Stiekemeheld <42800630+Stiekemeheld@users.noreply.github.com>
2021-01-05 21:13:43 +0100

    version 2.0

---
+++ Assets/background.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class background : MonoBehaviour
+{
+    private Rigidbody2D myRigidBody,koenibaldBody;
+    public GameObject Koenibald;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        koenibaldBody = Koenibald.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRigidBody.velocity = new Vector2(koenibaldBody.velocity.x / 4, 0);
+
+    }
+}
